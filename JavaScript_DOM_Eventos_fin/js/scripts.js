c4ca6cd6b936d554d644e5222c694766306fa598
GonzoTheOne63 <gbarron.ms@gmail.com>
2022-03-24 08:23:27 -0600

    CommitFinal
    Signed-off-by: GonzoTheOne63 <gbarron.ms@gmail.com>

--- JavaScript_DOM_Eventos_fin/js/scripts.js
+++ JavaScript_DOM_Eventos_fin/js/scripts.js
@@ -110,53 +110,31 @@ formulario.addEventListener("submit", function (e) {
   const { nombre, email, mensaje } = datos;
 
   if (nombre === "" || email === "" || mensaje === "") {
-    console.log("Al menos un campo esta vacio");
-    mostrarError("Todos los campos son obligatorios");
+    mostrarAlerta("Todos los campos son obligatorios", true);
     return; // Detiene la ejecución de esta función
   }
 
-  console.log("Todo bien...");
-
-  mostrarMensaje("Gracias, enviado correctamente");
+  // Crear ALERTA 'Enviado correctamente
+  mostrarAlerta("Gracias, enviado correctamente");
 });
 
-function mostrarError(mensaje) {
-  const alerta = document.createElement("p");
-  alerta.textContent = mensaje;
-  alerta.classList.add("error");
-
-  formulario.appendChild(alerta);
-
-  setTimeout(() => {
-    alerta.remove();
-  }, 3000);
-}
-
-function mostrarMensaje(mensaje) {
-  const alerta = document.createElement("p");
-  alerta.textContent = mensaje;
-  alerta.classList.add("correcto");
-  formulario.appendChild(alerta);
-
-  setTimeout(() => {
-    alerta.remove();
-  }, 3000);
-}
-
-// Eventos de los Inputs...
-const nombre = document.querySelector("#nombre");
-const email = document.querySelector("#email");
-const mensaje = document.querySelector("#mensaje");
-
-nombre.addEventListener("input", leerTexto);
-email.addEventListener("input", leerTexto);
-mensaje.addEventListener("input", leerTexto);
-
 function leerTexto(e) {
-  // console.log(e);
   // console.log(e.target.value);
-
   datos[e.target.id] = e.target.value;
-
-  console.log(datos);
+  //console.log(datos);  
+}
+function mostrarAlerta(mensaje, error = null) { 
+  const alerta = document.createElement("p");
+  alerta.textContent = mensaje;
+  
+  if(error) {
+      alerta.classList.add('error');
+  } else {
+      alerta.classList.add('correcto');
+  }
+  formulario.appendChild(alerta);
+  //Desaparezca luego de 5 segundos
+  setTimeout(() => {
+      alerta.remove();
+  }, 5000);
 }
